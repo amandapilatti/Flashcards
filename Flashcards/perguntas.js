a313fd1118907df753d7daa537d8384421a93f17
amandapilatti <143842040+amandapilatti@users.noreply.github.com>
2024-11-05 08:48:59 -0300

    Add files via upload

---
+++ Flashcards/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Ídol Jisoo',
+    'Qual sua comida favorita?',
+    'Curry, ela usa como acompanhamento.'
+)
+
+criaCartao(
+    'Ídol Jisoo',
+    'Em qual cidade ela nasceu?',
+    'Sanbon-dong, Gunpo, Coreia do Sul.'
+)
+
+criaCartao(
+    'Ídol Jisoo',
+    'Apelido de Jisoo?',
+    'Sooya.'
+)
