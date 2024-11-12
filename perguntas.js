f742bba4bf3061586bbe93cdf3c4cb3d50b8e4c2
amandapilatti <143842040+amandapilatti@users.noreply.github.com>
2024-11-12 10:39:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Jisoo',
+    'Qual sua comida favorita?',
+    'Curry, ela usa como acompanhamento.'
+)
+
+criaCartao(
+    'Jisoo',
+    'Em que cidade ela nasceu?',
+    'Sanbon-dong, Gunpo, Coreia do Sul.'
+)
+
+criaCartao(
+    'Jisoo',
+    'Apelido de Jisoo?',
+    'Sooya'
+)
+
+criaCartao(
+    'Jisoo',
+    'Sua data de nascimento?',
+    '03/01/1995'
+)
+
+criaCartao(
+    'Jisoo',
+    'Qual seu drama mais conhecido?',
+    'Snowdrop'
+)
+
+criaCartao(
+    'Jisoo',
+    'Apelido de Jisoo?',
+    'Sooya'
+)
+
+
+
